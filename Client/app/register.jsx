2738ec73603b954d703643f258f669880f7fa4ae
Raja <mrajasekhar9676@gmail.com>
2024-08-09 11:33:22 +0530

    change url

--- Client/app/register.jsx
+++ Client/app/register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
 
   const handleRegister = async () => {
     try {
-      const response = await axios.post('https://e89c-2401-4900-1f28-6e01-74c0-d168-60ec-e67.ngrok-free.app/api/users/register', {
+      const response = await axios.post('https://e0ec-2401-4900-1cc5-4d95-c861-271e-2c9f-bf7b.ngrok-free.app/api/users/register', {
         username,
         email,
         password,
